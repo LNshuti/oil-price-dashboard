e8b867bc8041a5a647ad878f367e5bdb8d34c136
Leonce Nshuti <LNshuti@users.noreply.github.com>
2022-10-04 21:00:53 -0500

    add required environment files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python dashboard for visualizing key macroeconomic series in the United States',
+    author='Leonce Nshuti',
+    license='MIT',
+)
