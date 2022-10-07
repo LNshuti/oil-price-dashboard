3c7f086e6a304461548b79efef5b02e1a7ffe315
Leonce Nshuti <LNshuti@users.noreply.github.com>
2022-10-06 22:45:20 -0500

    add models

--- backend/server/apps/endpoints/models.py
+++ backend/server/apps/endpoints/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
 # Create your models here.
+class Endpoint(models.Model):
+    """
+    The Endpoint object represents ML API endpoint
+
+    Attributes: 
+        name: The name of the endpoint, used in the API URL 
+        ownder: string with owner name 
+        created_at: date when endpoint was created
+    
+    """
+
+    name = models.CharField(max_length=128)
+    owner = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+class MLAlgorithm(models.Model): 
+    """
+    Attributes: 
+        name: name of the algorithm
+        description: short description of how the algorithm works 
+        code: code of the algorithm 
+        version: version of the algorithm 
+        created_at: date when algorithm was created 
+        parent_endpoint: Reference to the endpoint
+    """
+
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1000)
+    code = models.CharField(max_length=50000)
+    version = models.CharField(max_length=128)
+    owner = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
+
+class MLAlgorithmStatus(models.Model):
+    '''
+    The MLAlgorithmStatus represent status of the MLAlgorithm which can change during the time.
+
+    Attributes:
+        status: The status of algorithm in the endpoint. Can be: testing, staging, production, ab_testing.
+        active: The boolean flag which point to currently active status.
+        created_by: The name of creator.
+        created_at: The date of status creation.
+        parent_mlalgorithm: The reference to corresponding MLAlgorithm.
+
+    '''
+    status = models.CharField(max_length=128)
+    active = models.BooleanField()
+    created_by = models.CharField(max_length=128)
+    created_at = models.DateTimeField(max_length=128)
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE, related_name="status")
+
+class MLRequest(models.Model):
+    
+    """ 
+    The MLRequest will keep information about all requests to ML algorithms.
+
+    Attributes:
+        input_data: The input data to ML algorithm in JSON format.
+        full_response: The response of the ML algorithm.
+        response: The response of the ML algorithm in JSON format.
+        feedback: The feedback about the response in JSON format.
+        created_at: The date when request was created.
+        parent_mlalgorithm: The reference to MLAlgorithm used to compute response.
+    """
+    
+    input_data = models.CharField(max_length=10000)
+    full_response = models.CharField(max_length=10000)
+    response = models.CharField(max_length=10000)
+    feedback = models.CharField(max_length=10000, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE)
