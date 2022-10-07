9847c024823ac0098b94c787c6f60abd1cfe456a
Leonce Nshuti <LNshuti@users.noreply.github.com>
2022-10-07 00:06:41 -0500

    update models

--- backend/server/apps/ml/tests.py
+++ backend/server/apps/ml/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+import inspect
+from apps.ml.registry import MLRegistry
 
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
 
@@ -25,3 +27,22 @@ class MLTests(TestCase):
         self.assertEqual('OK', response['status'])
         self.assertTrue('label' in response)
         self.assertEqual('<=50K', response['label'])
+
+# add below method to MLTests class:
+def test_registry(self):
+    registry = MLRegistry()
+    self.assertEqual(len(registry.endpoints), 0)
+    endpoint_name = "income_classifier"
+    algorithm_object = RandomForestClassifier()
+    algorithm_name = "random forest"
+    algorithm_status = "production"
+    algorithm_version = "0.0.1"
+    algorithm_owner = "Piotr"
+    algorithm_description = "Random Forest with simple pre- and post-processing"
+    algorithm_code = inspect.getsource(RandomForestClassifier)
+    # add to registry
+    registry.add_algorithm(endpoint_name, algorithm_object, algorithm_name,
+                algorithm_status, algorithm_version, algorithm_owner,
+                algorithm_description, algorithm_code)
+    # there should be one endpoint available
+    self.assertEqual(len(registry.endpoints), 1)
