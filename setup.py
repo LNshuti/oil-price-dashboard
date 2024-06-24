f706f443a7ece7cb0662922e4e0c578cc9614395
LeonceNsh <nshutl0@sewanee.edu>
2024-06-23 23:09:40 -0500

    remove old files and reorganize repo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Python dashboard for visualizing key macroeconomic series in the United States',
+    description='Python dashboard for visualizing key oil price series in the United States',
     author='Leonce Nshuti',
     license='MIT',
 )
